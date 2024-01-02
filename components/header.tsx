82fe1e2585e851cdf489c6bf6ecfb869e2f458d2
albertcai101 <albertcai.paloalto@gmail.com>
2024-01-02 00:46:14 -0800

    back button

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,39 @@
+"use client";
 import Link from 'next/link';
 import { UserButton } from "@clerk/nextjs";
+import { useRouter, usePathname } from 'next/navigation';
 
 const Header = () => {
+    const router = useRouter();
+    const isHomePage = usePathname() === '/';
+
+    // CSS classes for the back button
+    const backButtonBaseClass = "mr-4";
+    const backButtonVisibleClass = isHomePage ? "invisible" : "visible stroke-current";
+    const backButtonHoverClass = "hover:stroke-[#b8a3f6]";
+
     return (
-        <div className="flex justify-between items-center w-full">
-            <div className="flex-grow text-center">
+        <div className="flex items-center justify-between w-full">
+            {/* Left Column - Always render the button but control visibility */}
+            <div className="flex justify-start">
+                <button onClick={() => router.back()} className={`${backButtonBaseClass} ${backButtonVisibleClass} ${backButtonHoverClass}`}>
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+                    </svg>
+                </button>
+            </div>
+
+            {/* Center Column */}
+            <div className="flex justify-center flex-grow">
                 <Link href="/">
                     <span className="gradient-header">Good Night No Devices</span>
                 </Link>
             </div>
-            <UserButton afterSignOutUrl="/" />
+
+            {/* Right Column */}
+            <div className="flex justify-end">
+                <UserButton afterSignOutUrl="/" />
+            </div>
         </div>
     );
 };
